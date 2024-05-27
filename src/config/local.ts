c0c469451442edac59b5b6a30493ea28fe42102c
Salim Salimzada <salimsalimzada2002@gmail.com>
2024-05-27 05:57:04 +0400

    Add more

--- src/config/local.ts
+++ src/config/local.ts
@@ -1,4 +1,4 @@
 export default {
   port: 3001,
-  baseUrl: "http://localhost:3001",
+  baseUrl: "http://localhost",
 };
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import * as dotenv from 'dotenv'
-import config from './config'
-import app from './server'
-dotenv.config()
-app.listen(config.port,() => {
-    console.log(`service is running on http://localhost:${config.port}`)
-})
+import * as dotenv from "dotenv";
+import config from "./config";
+import app from "./server";
+dotenv.config();
+app.listen(config.port, () => {
+  console.log(
+    `service is running on ${process.env.baseUrl}:${process.env.port}`
+  );
+});
